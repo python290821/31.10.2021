2f1f9f72f7773984d7dd1eef8726e77e99962d4c
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-31 19:53:41 +0200

    Create more_cm.py

---
+++ more_cm.py
@@ -0,0 +1,16 @@
+# 1
+# "i wake up at 6:00 am"
+# create a list containing only letters (hint: isalpha)
+# create a list containing only digits (hint: isdigit)
+# 2
+# [5.9, 0.9, -9.2, 11.1 ] -- create a list as ints
+# [5,   0,    -9, 11 ]
+# nums = [[1,2,6], [7,8,9], [100,5,2]]
+# create a list of the max [6, 9, 100]
+# create a list of the min [1, 7, 2]
+# create a list of avg 
+#import statistics
+#print(statistics.mean([1,2,3]))
+# 3
+# _prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
+# return prices bigger than 0, for lower than zero return 0
