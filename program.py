ea8a103af9132701f6793ca7b483e1ab33261b8a
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-31 21:30:56 +0200

    Update program.py

--- program.py
+++ program.py
@@ -7,6 +7,8 @@ _under = ['_' * len(_word) for _word in _phrase]
 print(_phrase)
 print(_under)
 
+# this is more complicated than a list of letters
+
 _letter = 'e' #input('enter a letter: ')
 #new_under = [_letter for _word in _phrase for _letter in _phrase]
 #print(new_under)
@@ -22,3 +24,17 @@ for _word_index in range(len(_phrase)):
     print()
 print(_under)
 
+_letter = 'o' #input('enter a letter: ')
+#new_under = [_letter for _word in _phrase for _letter in _phrase]
+#print(new_under)
+for _word_index in range(len(_phrase)):
+    new_word = ''
+    for _letter_index in range(len(_phrase[_word_index])):
+        print(_phrase[_word_index][_letter_index], end='')
+        if _phrase[_word_index][_letter_index] == _letter:
+            new_word += _letter
+        else:
+            new_word += _under[_word_index][_letter_index]
+    _under[_word_index] = new_word
+    print()
+print(_under)
